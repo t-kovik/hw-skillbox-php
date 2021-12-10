b3b81497ca96dd29cf98cc0ef959bcc37e04e983
t-kovik <tatyana.kovik@icloud.com>
2021-12-10 12:16:37 +0300

    module 5

---
+++ module_5/005_authors_and_books.php
@@ -0,0 +1,51 @@
+<pre>
+   <?php
+
+   $library = [
+       'authors' => [
+           'john_makkormik@example.com' => [
+               'name' => 'Джон Маккормик',
+               'email' => 'john_makkormik@example.com',
+               'yearOfBirth' => '1973',
+           ],
+           'martin_robert@example.com' => [
+               'name' => 'Мартин Роберт',
+               'email' => 'martin_robert@example.com',
+               'yearOfBirth' => '1985',
+           ],
+           'steven_king@example.com' => [
+               'name' => 'Стивен Кинг',
+               'email' => 'steven_king@example.com',
+               'yearOfBirth' => '1947',
+           ]
+       ],
+       'books' => [
+           [
+               'title' => 'Чистый код: создание, анализ и рефакторинг',
+               'author' => 'martin_robert@example.com',
+               'publicationDate' => '2008'
+           ],
+           [
+               'title' => 'Девять алгоритмов, которые изменили будущее',
+               'author' => 'john_makkormik@example.com',
+               'publication date' => '2014'
+           ],
+           [
+               'title' => 'Идеальный программист',
+               'author' => 'martin_robert@example.com',
+               'publication date' => '1996'
+           ],
+           [
+               'title' => 'Сияние',
+               'author' => 'steven_king@example.com',
+               'publication date' => '1977'
+           ],
+       ],
+   ];
+
+   foreach ($library['books'] as $item) {
+       var_dump("Книга {$item['title']}, её написал {$library['authors'][$item['author']]['name']} {$library['authors'][$item['author']]['yearOfBirth']} ({$item['author']})");
+   }
+   ?>
+</pre>
+
